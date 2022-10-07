e5e06203da2c2a9a296d3fa68cc9f6c83082be06
moe0925 <114910800+moe0925@users.noreply.github.com>
2022-10-07 11:26:39 +0900

    Add files via upload

---
+++ ft_tolower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int ft_tolower(int c)
+{
+    int c2;
+    if (c >= 65 && c <= 90)
+    {
+        c2 = c + 32;
+        return (c2);
+    }
+    else 
+        return (c);
+
+}
+
+int main ()
+{
+    int c;
+    // c = toupper('a');
+    // printf("aを大文字に変換→%c\n",c);
+    c = ft_tolower('A');
+    printf("Aを小文字に変換→%c\n",c);
+}
+
---
+++ ft_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int ft_toupper(int c)
+{
+    int c2;
+    if (c >= 97 && c <= 122)
+    {
+        c2 = c - 32;
+        return (c2);
+    }
+    else 
+        return (c);
+
+}
+
+int main ()
+{
+    int c;
+    // c = toupper('a');
+    // printf("aを大文字に変換→%c\n",c);
+    c = ft_toupper('a');
+    printf("aを大文字に変換→%c\n",c);
+}
+
